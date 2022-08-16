3c6fb605285a0375bf5e3f16c82b7dc73330fed4
e-ishikawa <orion.0424.aurora@gmail.com>
2022-08-16 10:57:39 +0000

    php

---
+++ practice.php
@@ -0,0 +1,47 @@
+<?php
+$a =3;
+$b =7;
+echo $a+$b;
+
+$array_manth=[
+    "january" => "1月",
+    "february" => "2月",
+    "march" => "3月",
+    "april" => "4月",
+    "may" => "5月",
+    "june" => "6月",
+    "july" => "7月",
+    "august" => "8月",
+    "september" => "9月",
+    "october" => "10月",
+    "november" => "11月",
+    "december" => "12月"
+    ];
+echo $array_manth["august"];
+
+$hello = "Hello";
+$name ="emi";
+$world ="'s World!";
+echo $hello.$name.$world;
+
+$tech_boost = 'tech';
+$tech_boost.='boost';
+echo $tech_boost;
+
+$calendar = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+// 12月を表示する
+echo $calendar["December"];
