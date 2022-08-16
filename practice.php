abae92809c4662a9078abcf24274dfc3e3e262cd
e-ishikawa <orion.0424.aurora@gmail.com>
2022-08-16 13:17:20 +0000

    php

--- practice.php
+++ practice.php
@@ -1,47 +1,25 @@
 <?php
-$a =3;
-$b =7;
-echo $a+$b;
+$name = "emi";
 
-$array_manth=[
-    "january" => "1月",
-    "february" => "2月",
-    "march" => "3月",
-    "april" => "4月",
-    "may" => "5月",
-    "june" => "6月",
-    "july" => "7月",
-    "august" => "8月",
-    "september" => "9月",
-    "october" => "10月",
-    "november" => "11月",
-    "december" => "12月"
-    ];
-echo $array_manth["august"];
+switch ($name) {
+  case "emi":
+    echo "私はemiです";
+    break;
+  default:
+    echo "emiではありません";
+    break;
+}
 
-$hello = "Hello";
-$name ="emi";
-$world ="'s World!";
-echo $hello.$name.$world;
+$total = 0;
 
-$tech_boost = 'tech';
-$tech_boost.='boost';
-echo $tech_boost;
+for ($i =0; $i <=10000; $i++) {
+    $total += $i;
+}
+echo $total;
 
-$calendar = [
-  "January" => "1月",
-  "February" => "2月",
-  "March" => "3月",
-  "April" => "4月",
-  "May" => "5月",
-  "June" => "6月",
-  "July" => "7月",
-  "August" => "8月",
-  "September" => "9月",
-  "October" => "10月",
-  "November" => "11月",
-  "December" => "12月"
-];
+$fruits = array("banana","apple","orange","peach","mango");
 
-// 12月を表示する
-echo $calendar["December"];
+foreach($fruits as $fruit) {
+    echo $fruit;
+    echo "\n";
+}
