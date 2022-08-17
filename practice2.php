66da217fe4671b02f774126a985901f050915186
e-ishikawa <orion.0424.aurora@gmail.com>
2022-08-17 15:04:59 +0000

    php7

--- practice2.php
+++ practice2.php
@@ -9,15 +9,16 @@ switch ($name) {
     echo "Emiではありません";
     break;
 }
-echo "\n";
+    echo "\n";
+
 
 $total = 0;
 
-for ($i = 0; $i <= 10000; $i ++) {
+for ($i =0; $i <=10000; $i++) {
     $total += $i;
 }
 echo $total;
-echo "\n";
+echo"\n";
 
 $fruits = array("banana","apple","orange","peach","mango");
 
@@ -26,15 +27,15 @@ foreach($fruits as $fruit) {
     echo "\n";
 }
 
-$i = 0;
 /* for文の始めの値を定義する */
 $start = 1;
 /* for文の終わりの値を定義する */
 $end = 100;
 
-for($i = $start; $i < $end; $i++){
-}
+for($i = $start; $i <= $end; $i++){
+
   // 5で割り切れたら{}内を実行する
-  if($i % 5 === 0){
-    echo $i."\n";
+  if($i % 5 == 0){
+    echo $i . "\n";
+  }
 }
