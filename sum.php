c2b206a8cb225a74ad3bba30aa68174968416347
e-ishikawa <orion.0424.aurora@gmail.com>
2022-08-21 13:43:27 +0000

    php11

--- sum.php
+++ sum.php
@@ -1,37 +1,34 @@
 <?php
-function num($num) {
+function times2($num){
     return $num *2;
 }
-echo num(5) ."\n";
+echo times2(5) ."\n";
 
-function add($a, $b) {
-     return $a + $b;
-}    
+function add($a, $b){
+    return $a + $b;
+}
 echo add(1,3) ."\n";
 
-
-
 function product($arr){
-     $result = $arr[0]; 
-     for ($i = 1; $i < count($arr); $i++) {
-             $result *= $arr[$i];
-     }
-     return $result;
+    $result = $arr[0];
+    for ($i = 1; $i < count($arr); $i++){
+        $result *= $arr[$i];
+    }
+    return $result;
 }
-echo product(array(1, 3, 5 ,7, 9)) ."\n";
+echo product(array(1, 3, 5, 7, 9)) ."\n";
 
 function max_array($arr){
-  // とりあえず配列の最初の要素を一番大きい値とする
+    /*とりあえず配列の最初の要素を一番大きい値とする*/
     $max_number = $arr[0];
-      foreach($arr as $a){
-      if($max_number < $a){
-        $max_number = $a;
+    foreach($arr as $a){
+        if($max_number < $a){
+            $max_number = $a;
+        }
     }
- }
     return $max_number;
- }
- echo max_array(array(6, 2, 3, 10, 5)) ."\n";
-
+}
+echo max_array(array(6, 2, 3, 10, 5)) ."\n";
 
 /*strip_tags*/
 /*HTMLタグを取り除く*/
@@ -72,12 +69,11 @@ $time1 = time();
  
 //3日後のタイムスタンプを取得
 $time2 = time() + (3 * 24 * 60 * 60);
- 
+
 if($time1 < $time2){
- echo '$time2が大きい値です' ."\n";
+    echo '$time2が大きい値です' ."\n";
 }
-
-
+ 
 /*mktime*/
 /*time関数と同じく、UNIXタイムスタンプを取得する関数*/
 /*time関数は現在時刻のUNIXタイムスタンプを取得することができる*/
@@ -86,7 +82,6 @@ if($time1 < $time2){
 //指定した日時のタイムスタンプを取得
 $timestamp = mktime(0, 0, 0, 8, 1, 2017);
 echo $timestamp ."\n";
-
 
 /*date*/
 /*指定された日時を任意の形式でフォーマットし、日付文字列を返す関数*/
